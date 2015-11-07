4c118b613e0ab96d4454564b84733f53f1bcdd91
Anthony <aferragano@gmail.com>
2015-11-07 15:53:48 -0500

    basic console log for click javascript

---
+++ _assets/javascripts/click.js
@@ -0,0 +1,17 @@
+$( document ).ready(function() {
+	$("#share-button").on("click", function(){
+			console.log("show new recipe modal")
+	})
+	$("#download-recipe-button").on("click", function(){
+			console.log("download recipe modal")
+	})
+
+	$("#download-placecard-button").on("click", function(){
+			console.log("download placecard modal")
+	})
+
+	$("#download-conversation-button").on("click", function(){
+			console.log("download conversation modal")
+	})
+
+});
