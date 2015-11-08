d0f2ed76657f4bba76cad59fc2cf7726c7cd464d
Anthony <aferragano@gmail.com>
2015-11-07 21:09:00 -0500

    modal show for create website

--- _assets/javascripts/click.js
+++ _assets/javascripts/click.js
@@ -1,15 +1,24 @@
 $( document ).ready(function() {
+	// create new recipe modal
 	$("#share-button").on("click", function(){
+		$("#create-recipe-form").show();
+		$("#veil").show();
+		$("#veil").on("click", function(){
+			$("#veil").hide();
+			$("#create-recipe-form").hide();
+		});
+
 			console.log("show new recipe modal")
 	})
+	// email and name modal for recipe download
 	$("#download-recipe-button").on("click", function(){
 			console.log("download recipe modal")
 	})
-
+	// email and name modal for placecard download
 	$("#download-placecard-button").on("click", function(){
 			console.log("download placecard modal")
 	})
-
+	// email and name modal for topic download
 	$("#download-conversation-button").on("click", function(){
 			console.log("download conversation modal")
 	})
