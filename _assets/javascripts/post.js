2697c6792cdc45971a6a7dc9eef042ccc12f2d34
Anthony <aferragano@gmail.com>
2015-11-09 14:07:22 -0500

    add unhrc logo file. trying groundwork

--- _assets/javascripts/post.js
+++ _assets/javascripts/post.js
@@ -13,44 +13,54 @@ function submitRecipe(){
 	$form = $("#create-recipe-form");
 
 	$form.submit(function(event){
-		console.log(event);
-		event.preventDefault();
 
-		$ajax({
-			type: "POST",
-			url: '//nyc.us11.list-manage.com/subscribe/post-json?u=7aa897cfc40f7cfbb83ffadd4&amp;id=c8e53459bc&c=?',
-			data: $form.serialize(),
-			timeout: 5000,
-			cache: false,
-			dataType: 'jsonp',
-			contentType: "application/json; charset=utf-8",
-			error: function(err) {console.log("Error.")},
-			success: function(data){
-				if (data.result != "success") {
-					console.log("error")
-				} else {
-					console.log("success")
-				}
-			}
+		console.log($form);
+		event.preventDefault();
+		data = {
+		  email: email,
+		  source: "friendsgiving recipe",
+		  tags:{
+		    recipeTitle:"title",
+		    recipeInstructions: "etc",
+		    sendEmail: 0
+		    }
+		  }
+
+		// $ajax({
+		// 	type: "POST",
+		// 	url: '//nyc.us11.list-manage.com/subscribe/post-json?u=7aa897cfc40f7cfbb83ffadd4&amp;id=c8e53459bc&c=?',
+		// 	data: $form.serialize(),
+		// 	timeout: 5000,
+		// 	cache: false,
+		// 	dataType: 'jsonp',
+		// 	contentType: "application/json; charset=utf-8",
+		// 	error: function(err) {console.log("Error.")},
+		// 	success: function(data){
+		// 		if (data.result != "success") {
+		// 			console.log("error")
+		// 		} else {
+		// 			console.log("success")
+		// 		}
+		// 	}
 		})
 	})
 }
 
-function sendData(id,network,gw){
-  var data = {
-    source: "friendsgiving signup",
-    email: "junk@junk.com",
-    tags: {
-      id: id,
-      network: network
-    }
-  }
-  console.log(data);
-  gw.supporters.create(data)
-  .then(function(res){
-    console.log(res);
-  })
-  .catch(function(res){
-    console.log(res);
-  });
-};
+// function sendData(id,network,gw){
+//   var data = {
+//     source: "friendsgiving signup",
+//     email: "junk@junk.com",
+//     tags: {
+//       id: id,
+//       network: network
+//     }
+//   }
+//   console.log(data);
+//   gw.supporters.create(data)
+//   .then(function(res){
+//     console.log(res);
+//   })
+//   .catch(function(res){
+//     console.log(res);
+//   });
+// };
