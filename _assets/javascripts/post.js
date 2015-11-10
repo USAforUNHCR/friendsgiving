4ef8c601783e9399456595afdc14b335dc3e4542
Anthony <aferragano@gmail.com>
2015-11-09 20:27:52 -0500

    form hook up header sign up . share recipe signup . download recipe signup . placecard sign up . conversation sign up.

--- _assets/javascripts/post.js
+++ _assets/javascripts/post.js
@@ -8,7 +8,11 @@ $(document).ready(function(){
 
   // submitRecipe();
   var id = getId();
-  shareListener(id,gw);
+  shareRecipe(id,gw);
+  shareHeader(id,gw);
+  recipeSignup(id,gw);
+  placeCardSignup(id,gw);
+  topicSignup(id,gw);
 
 
 });
@@ -57,20 +61,58 @@ function getId(){
     return -1
   }
 }
-function shareListener(id,gw){
+//1 share recipe
+function shareRecipe(id,gw){
+  $('.email-signup').submit(function(event){
+  	console.log($('.email-signup'))
+    event.preventDefault();
+    var network = $(event.target).attr('class');
+    sendRecipeData(id,network,gw);
+  });
+
+}
+
+function sendRecipeData(id,network,gw){
+  var data = {
+    source: "friendsgiving header signup",
+    email: $('.email-signup')[0][0].value,
+    name: $('.email-signup')[0][1].value,
+    mobile: $('.email-signup')[0][2].value,
+    tags: {
+      // email: $('.email-signup')[0][0].value,
+      // name: $('.email-signup')[0][1].value,
+      // recipeName: $('.email-signup')[0][2].value,
+      // instructions: $('.email-signup')[0][3].value,
+      // imageLink: $('.email-signup')[0][4].value,
+    }
+  }
+  console.log(data);
+  gw.supporters.create(data)
+  .then(function(res){
+    console.log(res);
+    console.log("success")
+  })
+  .catch(function(res){
+    console.log(res);
+    console.log("failure")
+    $("#share-recipe-error").append("sorry an error has occured")
+  });
+};
+// 2 header sigup
+function shareHeader(id,gw){
   $('#create-recipe-form').submit(function(event){
   	console.log('#create-recipe-form')
   	console.log($('#create-recipe-form'))
     event.preventDefault();
     var network = $(event.target).attr('class');
-    sendData(id,network,gw);
+    sendHeaderData(id,network,gw);
   });
 
 }
 
-function sendData(id,network,gw){
+function sendHeaderData(id,network,gw){
   var data = {
-    source: "friendsgiving",
+    source: "friendsgiving share recipe",
     email: "test@test.com",
     tags: {
       email: $('#create-recipe-form')[0][0].value,
@@ -84,8 +126,110 @@ function sendData(id,network,gw){
   gw.supporters.create(data)
   .then(function(res){
     console.log(res);
+    console.log("success")
   })
   .catch(function(res){
     console.log(res);
+    console.log("failure")
+    $("#header-error").append("we're sorry, an error has occured.")
+  });
+};
+
+// 3 downloadRecipe sigup
+function recipeSignup(id,gw){
+  $('#download-recipe-form').submit(function(event){
+  	console.log($('#download-recipe-form'))
+    event.preventDefault();
+    var network = $(event.target).attr('class');
+    sendRecipeSignup(id,network,gw);
+  });
+
+}
+
+function sendRecipeSignup(id,network,gw){
+  var data = {
+    source: "friendsgiving download-recipes",
+    email: $('#download-recipe-form')[0][1].value,
+    name: $('#download-recipe-form')[0][0].value,
+    tags: {
+      // email: $('#download-recipe-form')[0][1].value,
+      // name: $('#download-recipe-form')[0][0].value
+    }
+  }
+  console.log(data);
+  gw.supporters.create(data)
+  .then(function(res){
+    console.log(res);
+    console.log("success")
+  })
+  .catch(function(res){
+    console.log(res);
+    console.log("failure")
+    $("#download-recipe-error").append("We're sorry, an error has occured.")
+  });
+};
+
+//4 PLACECARD signup
+function placeCardSignup(id,gw){
+  $('#placecard-form').submit(function(event){
+  	console.log($('#placecard-form'))
+    event.preventDefault();
+    var network = $(event.target).attr('class');
+    sendPlaceCardSignup(id,network,gw);
+  });
+
+}
+
+function sendPlaceCardSignup(id,network,gw){
+  var data = {
+    source: "friendsgiving placecard signup",
+    email: $('#placecard-form')[0][1].value,
+    name: $('#placecard-form')[0][0].value,
+    tags: {
+    }
+  }
+  console.log(data);
+  gw.supporters.create(data)
+  .then(function(res){
+    console.log(res);
+    console.log("success")
+  })
+  .catch(function(res){
+    console.log(res);
+    console.log("failure")
+    $("#placecard-error").append("We're sorry, an error has occured.")
+  });
+};
+
+
+// 5 Topic signup
+function topicSignup(id,gw){
+  $('#topic-form').submit(function(event){
+  	console.log($('#topic-form'))
+    event.preventDefault();
+    var network = $(event.target).attr('class');
+    sendTopicSignup(id,network,gw);
+  });
+
+}
+
+function sendTopicSignup(id,network,gw){
+  var data = {
+    source: "friendsgiving topic signup",
+    email: $('#topic-form')[0][1].value,
+    name: $('#topic-form')[0][0].value,
+    tags: {
+    }
+  }
+  console.log(data);
+  gw.supporters.create(data)
+  .then(function(res){
+    console.log(res);
+    console.log("success")
+  })
+  .catch(function(res){
+    console.log(res);
+    console.log("failure")
+    $("#placecard-error").append("We're sorry, an error has occured.")
   });
 };
